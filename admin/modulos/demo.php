fef9a8df3b2a762cbdde6cb211bad306d1763130
TC-tdea-2020 <TC-tdea-2020@outlook.es>
2020-06-17 00:55:47 -0500

    Cambio

--- admin/modulos/demo.php
+++ admin/modulos/demo.php
@@ -115,7 +115,7 @@ imagedestroy($bg);
 
 
 
-<form method="post" action="../facturas/facturas.php" enctype="multipart/form-data">
+<form method="post" action="facturas/facturas.php" enctype="multipart/form-data">
     <button type="submit">GENERAR FACTURA PDF</button>
     
     <h3>Los datos de la factura demo son los siguientes ...</h3>
